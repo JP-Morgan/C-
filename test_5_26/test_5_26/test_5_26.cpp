ff1aac2a5009dbd8a0c14389f889ccbea6fd9cb2
morgan5250 <2509071851@qq.com>
2023-05-26 20:05:20 +0800

    主要代码在Linux——进程控制

---
+++ test_5_26/test_5_26/test_5_26.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <Windows.h>
+//主要代码在Linux上
+int main()
+{
+	printf("hello");
+	Sleep(10);
+	exit(EXIT_SUCCESS);
+	//return 0;
+}
