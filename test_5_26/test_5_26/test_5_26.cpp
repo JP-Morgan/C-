e27e85ce0ed69c7c026e4b6d85c9f7e5e6a2d866
morgan5250 <2509071851@qq.com>
2023-05-30 21:46:36 +0800

    更新

--- test_5_26/test_5_26/test_5_26.cpp
+++ test_5_26/test_5_26/test_5_26.cpp
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <Windows.h>
 //主要代码在Linux上
+//int main()
+//{
+//	printf("hello");
+//	Sleep(10);
+//	exit(EXIT_SUCCESS);
+//	//return 0;
+//}
+int Fun()
+{
+    printf("fun test\n");
+    return 1;
+}
 int main()
 {
-	printf("hello");
-	Sleep(10);
-	exit(EXIT_SUCCESS);
-	//return 0;
+    printf("ennenen");
+    _exit(12);
+    //return 0;
 }
